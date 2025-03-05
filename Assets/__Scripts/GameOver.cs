a9d903f4bef22298ebc16c1572027bfe7cd8b334
kkc76-RU <kkc76@scarletmail.rutgers.edu>
2025-03-04 22:11:07 -0500

    Clean up
    I rid of code that is cogging up the script

--- Assets/__Scripts/GameOver.cs
+++ Assets/__Scripts/GameOver.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 public class GameOver : MonoBehaviour
 {
-	public Text countText;
-	private int count;
 	void OnTriggerEnter(Collider other)
 	{
 		// when the trigger is hit by something
@@ -17,13 +14,7 @@ public class GameOver : MonoBehaviour
 
 
 		}
+		
 	}
-	void GetCountText()
-		{
-			// Update the text field of our 'countText' variable
-			countText.text = "Count: " + count.ToString();
 
-			// Check if our 'count' is equal to or exceeded 12
-
-		}
 }
